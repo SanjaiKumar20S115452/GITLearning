3b896236e18cd6f4c96d9a17d01ebea9eaf5f922
Sanjai Kumar Chandra Mohan <sanjaiedu6312@gmail.com>
2025-01-28 07:58:14 -0800

    Added Changes and sheet updation done!

--- GitRepository/ContentView.swift
+++ GitRepository/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSush = false
     var body: some View {
         NavigationStack {
             VStack {
                 NavigationLink(destination: GitNewView()) {
                     Text("Git Learning App!")
                 }
+                Button {
+                    
+                }label: {
+                    Text("Show Sush")
+                }
+            }
+            .sheet(isPresented: $showSush) {
+                ShowSush()
             }
         }
         Text("Hey there!")
---
+++ GitRepository/ShowSush.swift
@@ -0,0 +1,18 @@
+//
+//  ShowSush.swift
+//  GitRepository
+//
+//  Created by Sanjai Kumar on 28/01/25.
+//
+
+import SwiftUI
+
+struct ShowSush: View {
+    var body: some View {
+        Text("Came from GIT sheet tab!")
+    }
+}
+
+#Preview {
+    ShowSush()
+}
