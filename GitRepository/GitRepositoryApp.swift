2be7a91a5a200aefb202aa6f6d652ebd288d0f3c
Sanjai Kumar Chandra Mohan <sanjaiedu6312@gmail.com>
2025-01-28 07:15:05 -0800

    Initial Commit

---
+++ GitRepository/GitRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepositoryApp.swift
+//  GitRepository
+//
+//  Created by Sanjai Kumar on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GitRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitRepositoryTests/GitRepositoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepositoryTests.swift
+//  GitRepositoryTests
+//
+//  Created by Sanjai Kumar on 28/01/25.
+//
+
+import Testing
+@testable import GitRepository
+
+struct GitRepositoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
